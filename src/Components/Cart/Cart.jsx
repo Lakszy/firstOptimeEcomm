cd46900b7e039db52aa3184e7d5e940ded748a70
Lakszy <lakshaykhattar0208@gmail.com>
2023-10-16 14:20:00 +0530

    fifth

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -17,10 +17,9 @@ import { removeFromCart } from "../../features/slideSlicer/cartSlice";
 const Cart = ({ openModal, setOpen }) => {
   const cart = useSelector((state) => state.cart.cart);
   const totalPrice = useSelector((state) => state.cart.totalPrice);
-  
-  const navigate = useNavigate(); 
-
+  const navigate = useNavigate();
   const dispatch = useDispatch();
+
   return (
     <div>
       {cart.length > 0 ? (
@@ -35,75 +34,74 @@ const Cart = ({ openModal, setOpen }) => {
             }}
           >
             <DialogHeader>Shopping Bag</DialogHeader>
-            <DialogBody
-              divider
-              className="flex flex-col justify-center items-start"
-            >
-              {cart.map((item, index) => {
-                return (
-                  <div key={index}>
-                    <div className="grid grid-cols-2 py-4">
-                      <div>
-                        <img
-                          className="h-[125px] rounded-md"
-                          src={item.img}
-                          alt={item.name}
-                        ></img>
-                        <div className="flex flex-col items-start">
-                          <h4 className="text-black text-base font-inter font-bold tracking-normal leading-none pt-2">
-                            {item.name}
-                          </h4>
+            <DialogBody divider>
+              <div style={{ maxHeight: '60vh', overflowY: 'auto' }}>
+                {cart.map((item, index) => {
+                  return (
+                    <div key={index}>
+                      <div className="grid grid-cols-2 py-4">
+                        <div>
+                          <img
+                            className="h-[125px] rounded-md"
+                            src={item.img}
+                            alt={item.name}
+                          ></img>
+                          <div className="flex flex-col items-start">
+                            <h4 className="text-black text-base font-inter font-bold tracking-normal leading-none pt-2">
+                              {item.name}
+                            </h4>
+                          </div>
+                          <div className="max-w-xs">
+                            <p className="text-black text-xs font-inter tracking-normal leading-none pt-2">
+                              {item.text}
+                            </p>
+                          </div>
                         </div>
-                        <div className="max-w-xs">
-                          <p className="text-black text-xs font-inter tracking-normal leading-none pt-2">
-                            {item.text}
+                        <div>
+                          <p className="text-black text-sm font-inter tracking-normal leading-none pt-2">
+                            Selected size:{" "}
+                            <span className="ml-2">{item.size}</span>
                           </p>
-                        </div>
-                      </div>
-                      <div>
-                        <p className="text-black text-sm font-inter tracking-normal leading-none pt-2">
-                          Selected size:{" "}
-                          <span className="ml-2">{item.size}</span>
-                        </p>
-                        <p className="text-black text-sm font-inter tracking-normal leading-none pt-2">
-                          Selected color:{" "}
-                          <span
-                            className="ml-2 rounded-full px-2"
-                            style={{ backgroundColor: item.color }}
-                          ></span>
-                        </p>
-                        <p className="text-black text-sm font-inter tracking-normal leading-none pt-2">
-                          Amount: <span className="ml-2">{item.amount}</span>
-                        </p>
-                        <p className="text-black text-sm font-inter tracking-normal leading-none pt-2">
-                          Single Item Price:{" "}
-                          <span className="ml-2">{item.price}$</span>
-                        </p>
-                        <p className="text-black text-sm font-inter tracking-normal leading-none pt-2">
-                          Total Item Prices:{" "}
-                          <span className="ml-2">{item.totalPrice}$</span>
-                        </p>
-                        <div className="pt-4">
-                          <Tooltip
-                            content="Remove from the Cart"
-                            placement="bottom"
-                          >
-                            <Button
-                              onClick={() => dispatch(removeFromCart(item))}
-                              size="sm"
-                              color="red"
-                              ripple={true}
-                              variant="filled"
+                          <p className="text-black text-sm font-inter tracking-normal leading-none pt-2">
+                            Selected color:{" "}
+                            <span
+                              className="ml-2 rounded-full px-2"
+                              style={{ backgroundColor: item.color }}
+                            ></span>
+                          </p>
+                          <p className="text-black text-sm font-inter tracking-normal leading-none pt-2">
+                            Amount: <span className="ml-2">{item.amount}</span>
+                          </p>
+                          <p className="text-black text-sm font-inter tracking-normal leading-none pt-2">
+                            Single Item Price:{" "}
+                            <span className="ml-2">{item.price}$</span>
+                          </p>
+                          <p className="text-black text-sm font-inter tracking-normal leading-none pt-2">
+                            Total Item Prices:{" "}
+                            <span className="ml-2">{item.totalPrice}$</span>
+                          </p>
+                          <div className="pt-4">
+                            <Tooltip
+                              content="Remove from the Cart"
+                              placement="bottom"
                             >
-                              Remove
-                            </Button>
-                          </Tooltip>
+                              <Button
+                                onClick={() => dispatch(removeFromCart(item))}
+                                size="sm"
+                                color="red"
+                                ripple={true}
+                                variant="filled"
+                              >
+                                Remove
+                              </Button>
+                            </Tooltip>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                );
-              })}
+                  );
+                })}
+              </div>
             </DialogBody>
             <DialogFooter className="flex justify-start items-center">
               <p className="text-black font-inter tracking-normal leading-none pt-2 font-bold text-lg justify-between">
@@ -112,7 +110,7 @@ const Cart = ({ openModal, setOpen }) => {
               </p>
               <button
                 onClick={() => navigate('/checkout/lakszy')}
-                class="hover:scale-95 mt-10 ml-40 md:mt-1 md:ml-24 flex select-none items-center gap-3 rounded-lg bg-gradient-to-tr from-blue-700 to-blue-300 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md transition-all active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                className="hover:scale-95 mt-10 ml-40 md:mt-1 md:ml-24 flex select-none items-center gap-3 rounded-lg bg-gradient-to-tr from-blue-700 to-blue-300 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md transition-all active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
                 type="button"
                 data-ripple-light="true"
               >
@@ -120,14 +118,14 @@ const Cart = ({ openModal, setOpen }) => {
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke-width="2"
+                  strokeWidth="2"
                   stroke="currentColor"
                   aria-hidden="true"
-                  class="h-5 w-5"
+                  className="h-5 w-5"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M14 5l7 7m0 0l-7 7m7-7H3"
                   ></path>
                 </svg>
@@ -153,7 +151,7 @@ const Cart = ({ openModal, setOpen }) => {
                 <h1 className="text-black text-3xl font-inter font-bold tracking-normal leading-none py-4">
                   Your bag is empty
                 </h1>
-                <p className="text-black text-base font-inter tracking-normal leading-none ">
+                <p className="text-black text-base font-inter tracking-normal leading-none">
                   Add some products
                 </p>
               </div>
